d108aa906619bf8e5d89fad227b624571fcf31b9
5ee5 <marks.sislovs@gmail.com>
2025-12-14 20:00:20 +0200

    Initial Commit

---
+++ Age/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+
+fn main() {
+    let current_year = 2025;
+
+    // Input name
+    println!("Enter your name: ");
+    let mut name = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+    let name = name.trim(); // remove newline
+
+    // Input year of birth
+    println!("Enter your year of birth: ");
+    let mut year_of_birth_str = String::new();
+    io::stdin()
+        .read_line(&mut year_of_birth_str)
+        .expect("Failed to read line");
+    let year_of_birth: i32 = year_of_birth_str
+        .trim()
+        .parse()
+        .expect("Please enter a valid number");
+
+    let age = current_year - year_of_birth;
+
+    println!("Hello, {}", name);
+    println!("You are {} years old!", age);
+}
